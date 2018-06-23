97ac2cbe8812f93a2fe123965bb5f345af72b785
chenhaohao <chenhaohao@chenhaohaodeMacBook-Air.local>
2018-06-23 21:51:26 +0800

    web

---
+++ web/web全栈/demo/meResult.php
@@ -0,0 +1,5 @@
+<?php
+    $data = $_POST['data'];
+    echo "用户名：".$data['name'];
+    echo "密码：".$data['password'];
+?>
---
+++ web/web全栈/demo/useMysql.php
@@ -0,0 +1,17 @@
+<?php
+    // $con = mysql_connect('127.0.0.1','root','dabingjiayiqie');
+    $con = mysqli_connect('127.0.0.1','root','dabingjiayiqie','mysql');
+
+    // $con = mysql_connect('127.0.0.1','root','');
+    // if (!$con){
+    //     die('Could not connect: ' . mysql_error());
+    // }
+
+    // mysql_select_db("mysql", $con)
+    // $sql = 'show databases'
+    // mysql_query($sql,$con)
+    // mysql_close($con)
+    // http://student.kaikeba.com/live/1535/web
+    // http://127.0.0.1/web%E5%85%A8%E6%A0%88/demo/useMysql.php
+    // Cookie: Hm_lvt_5caa30e0c191a1c525d4a6487bf45a9d=1529124985; NTKF_T2D_CLIENTID=guest49269639-C25D-A720-C49B-06F39A7758DD
+?>
